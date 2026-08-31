78b106b30308187df31513fb9f37e2dfc167ac78
4ldwin88 <thinhjay.nguyen@gmail.com>
2026-08-31 00:44:17 -0400

    Remove obsolete Discovery compatibility engine

--- intelligence/discovery/round3-engine.js
+++
@@ -1,3 +0,0 @@
-// Deprecated compatibility shim. Canonical implementation: ./discovery-engine.js (Discovery v3).
-export * from './discovery-engine.js';
-export {default} from './discovery-engine.js';
