f15db4b4d66f0aed886a02bf1e13db34cae6dfc2
4ldwin88 <thinhjay.nguyen@gmail.com>
2026-09-02 17:30:59 -0400

    Reset Discovery to canonical pre-stable version

--- intelligence/discovery/discovery-controller.js
+++ intelligence/discovery/discovery-controller.js
@@ -4,7 +4,8 @@ import {selectNextQuestion} from './question-scheduler.js';
 import {needsTriage,buildTriageQuestion} from './triage.js';
 import {stoppingDecision,handoffAudit} from './sufficiency.js';
 import {isConstructId} from '../../registries/taxonomy/index.js';
-export const DISCOVERY_VERSION='v9.01';
+import {ENGINE_VERSIONS} from '../version.js';
+export const DISCOVERY_VERSION=ENGINE_VERSIONS.discovery;
 const CONFIDENCE_ORDER=Object.freeze({LIMITED:1,MODERATE:2,WELL_SUPPORTED:3});
 function confidenceRank(state={}){return CONFIDENCE_ORDER[state.qualitativeConfidence]??0}
 function validConstructs(ids=[]){return [...new Set(ids.filter(isConstructId))]}
